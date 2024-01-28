11a66484d66ce3a4898b055a3dc3d0048667a50d
Jong-Min-Moon <jm.moon@yonsei.ac.kr>
2024-01-27 23:20:51 -0800

    bm

--- SDPNAL+v1.0/kmeans_sdp.m
+++ SDPNAL+v1.0/kmeans_sdp.m
@@ -1,5 +1,6 @@
+% Author:       Mixon, Villar, Ward.
 % Filename:     kmeans_sdp.m
-% Last edited:  9 May 2016 
+% Last edited:  2024-1-20 
 % Description:  This function uses SDPNAL+ [3] to solve the Peng and Wei's 
 %               kmeans SDP solution [2] according to the formulation in [1].
 % Inputs:       
@@ -12,6 +13,7 @@
 %               -k: 
 % 
 %               The number of clusters.
+%
 % Outputs:
 %               -X:
 %               
@@ -29,22 +31,21 @@
 %       constraints.
 % -------------------------------------------------------------------------
 
-function X=kmeans_sdp_1(P, k)
+function X=kmeans_sdp(P, k)
 
 N=size(P,2);
 
-%construction of distance squared matrix.
+% Construction of distance squared matrix.
 D = -P'*P;
 
-%SDP definition for SDPNAL+
+% SDP definition for SDPNAL+
 n=N;
 C{1}=D;
 blk{1,1}='s'; blk{1,2}=n;
 b=zeros(n+1,1);
 Auxt=spalloc(n*(n+1)/2, n+1, 5*n);
 Auxt(:,1)=svec(blk(1,:), eye(n),1);
-%Auxt(:,1) = svec(blk(1,:), zeros(n),1);
-%b(1,1)=0;
+
 b(1,1)=k;
 idx=2;
 for i=1:n
@@ -57,12 +58,13 @@ for i=1:n
 end
 At{1}=sparse(Auxt);
 
-OPTIONS.maxiter=1000;
-OPTIONS.tol=1e-6;
+OPTIONS.maxiter = 50000;
+OPTIONS.tol = 1e-6;
 OPTIONS.printlevel = 0;
 
-%SDPNAL+ call
+% SDPNAL+ call
 [obj,X,s,y,S,Z,y2,v,info,runhist]=sdpnalplus(blk,At,C,b,0,[],[],[],[],OPTIONS);
 
 X=cell2mat(X);
+
 end
