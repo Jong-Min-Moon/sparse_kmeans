583fa80d62f67336e5b512b009c45aca7144876d
Jong-Min-Moon <jm.moon@yonsei.ac.kr>
2024-03-06 21:44:40 -0800

    isee

--- experiment/14_02_16_2024/ISEE/rho45_Delta4_p100.m
+++ experiment/14_02_16_2024/ISEE/rho45_Delta4_p100.m
@@ -1,6 +1,9 @@
-rho = 20;
-p = 400
-Delta = 5
+rho = 45;
+Delta = 4
+p = 100
+
+
+
 
 %p=
 %Delta=
@@ -12,7 +15,7 @@ rho = rho /100
 
 
 s = 10;
-n_rep = 20;
+n_rep = 100;
 
 
 n=500;
@@ -23,32 +26,17 @@ n_iter = 6;
 
 
 
-clustering_acc_mat = repelem(0, n_rep );
 
 
 
 
 
 
-Omega = zeros([p,p]);
 
-for j=1:p
-    for l = 1:p
-        if j==l
-            Omega(j,l) = 1;
-        elseif abs(j-l) ==1
-            Omega(j,l) = rho;
-        end
-    end
-end
+Omega = eye(p) + diag(rho*ones(p-1,1), 1) + diag(rho*ones(p-1,1), -1);
 
 
 
-try chol(Omega);
-    disp('Matrix is symmetric positive definite.')
-catch ME
-    disp('Matrix is not symmetric positive definite')
-end
 
 Sigma = inv(Omega);
 M = Delta/2/ sqrt( sum( Sigma(1:s,1:s),"all") )
@@ -61,7 +49,7 @@ mu_2 = mu_0;
 beta = Omega * (mu_1-mu_2);
 fprintf( "delta confirmed: %f", sqrt( (mu_1-mu_2)' * beta ))
 norm((mu_1-mu_2))
-tic
+
 
 
 
@@ -71,19 +59,30 @@ mu_2_mat = repmat(mu_2, 1, n/2);%each column is one observation
 x_noiseless = [ mu_1_mat  mu_2_mat ];%each column is one observation 
 x_noisy = x_noiseless +  mvnrnd(zeros(p,1), Sigma, n)';
 
+
+clustering_acc_mat = zeros(n_rep);
+diff_x_tilde = zeros(n_rep, n_iter);
+diff_omega_diag = zeros(n_rep, n_iter);
+false_discov = zeros(n_rep, n_iter);
+true_discov = zeros(n_rep, n_iter);
+false_discov_top5 = repmat("0", [n_rep, n_iter]);
+omega_est_time = zeros(n_rep, n_iter);
+sdp_solve_time = zeros(n_rep, n_iter);
+
 for j = 1:n_rep
     fprintf("iteration: (%i)th \n\n", j)
-    rng(j)
-    tic
+    rng(j);
+
     %data generation
-    
     x_noisy = x_noiseless +  mvnrnd(zeros(p,1), Sigma, n)';%each column is one observation
-    clustering_acc_mat(j) = iterative_kmeans_spectral_init_sample_cov(x_noisy, K, s,n_iter, cluster_true, 'hc', false, 'basic');
+    
+
+    [clustering_acc_mat(j), diff_x_tilde(j,:), diff_omega_diag(j,:), false_discov(j,:), true_discov(j,:), false_discov_top5(j,:), omega_est_time(j,:), sdp_solve_time(j,:)]= iterative_kmeans_spectral_init_ISEE(x_noisy, K,n_iter, Omega, s, cluster_true, 'hc', true, 'basic');
     acc_so_far =  clustering_acc_mat(1:j);
     fprintf( "mean acc so far: %f\n",  mean( acc_so_far ) );
 
-    toc
+
         % iterate        
 end
 
-csvwrite(path_result, clustering_acc_mat)
+
