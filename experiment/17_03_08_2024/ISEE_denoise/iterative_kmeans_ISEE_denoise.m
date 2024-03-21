3e0ee7b6c54db81c6e1b10dcd614b8347c46c34d
Jong-Min-Moon <jm.moon@yonsei.ac.kr>
2024-03-21 00:02:59 -0700

    loglogn to logn, obj val

--- experiment/17_03_08_2024/ISEE_denoise/iterative_kmeans_ISEE_denoise.m
+++ experiment/17_03_08_2024/ISEE_denoise/iterative_kmeans_ISEE_denoise.m
@@ -1,12 +1,11 @@
-function [cluster_est, diff_x_tilde, diff_omega_diag, entries_survived, omega_est_time, sdp_solve_time] = iterative_kmeans_ISEE_denoise(x, K, n_iter, Omega, omega_sparsity, init_method) 
+function [cluster_est, diff_x_tilde, diff_omega_diag, entries_survived, omega_est_time, sdp_solve_time, obj_val] = iterative_kmeans_ISEE_denoise(x, K, n_iter, Omega, omega_sparsity, init_method) 
 
     % modified 03/07/2024
     n     = size(x,2);
     p     = size(x,1);
-    %thres = sqrt(2 * log(p))
     lambda = sqrt(log(p)/n);
-    diverging_quantity = sqrt(log(log(n)));
-    thres = diverging_quantity*max(omega_sparsity*lambda^2, lambda)
+    diverging_quantity = sqrt(log(n));
+    thres = diverging_quantity*max(omega_sparsity*lambda^2, lambda);
 
     fprintf("thres=%f", thres)
     Omega_x = Omega * x;
@@ -16,6 +15,7 @@ function [cluster_est, diff_x_tilde, diff_omega_diag, entries_survived, omega_es
     omega_est_time   = zeros(n_iter, 1);
     sdp_solve_time   = zeros(n_iter, 1);
     entries_survived = zeros(n_iter, p);
+    obj_val          = zeros(n_iter, p);
     cluster_est      = zeros(n_iter+1, n);
     
     %initialization
@@ -44,7 +44,7 @@ function [cluster_est, diff_x_tilde, diff_omega_diag, entries_survived, omega_es
         
         % 2. threshold the data matrix
         signal_est_now = mean( mean_now(:, cluster_est_now==1), 2) - mean( mean_now(:, cluster_est_now==-1), 2);   
-        abs_diff = abs(signal_est_now);%./sqrt(Omega_diag_hat) * sqrt( n_g1_now*n_g2_now/n );
+        abs_diff = abs(signal_est_now);
         s_hat = abs_diff > thres;
         x_tilde_now_s  = x_tilde_now(s_hat,:);
         entries_survived(iter,:) = s_hat;
