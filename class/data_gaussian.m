d98a492970e9dccc5615d246e965844b3d48fa0d
Jong-Min-Moon <jm.moon@yonsei.ac.kr>
2024-03-27 21:54:35 -0700

    get_cluster_mean_sparse

--- class/data_gaussian.m
+++ class/data_gaussian.m
@@ -1,26 +1,51 @@
-classdef data_gaussian
+classdef data_gaussian < handle
     properties
         dimension
         sample_size
         data
-        sample_covariance
-        entries_survived
+        covariance_full
+        entries_survived_boolean
+        number_entries_survived
         cluster_assign
         number_cluster
+        cluster_mean_mat
+        cluster_mean_sparse_mat
     end
 
     methods
-        function obj = data_gaussian(data_matrix)
-            obj.data = data_matrix;
-            obj.dimension = size(obj.data,1);
-            obj.sample_size = size(obj.data,2);
-            obj.entries_survived = 1:obj.dimension;
-            obj.number_cluster = 1;
+        function dg = data_gaussian(data_matrix)
+            dg.data = data_matrix;
+            dg.dimension = size(dg.data,1);
+            dg.sample_size = size(dg.data,2);
+            dg.entries_survived_boolean = repelem(true, dg.dimension);
+            dg.number_entries_survived = sum(dg.entries_survived_boolean);
+            dg.number_cluster = 1;
+            dg.cluster_assign = repelem(1,dg.sample_size);
         end
         
-        function set_cluster_assign(cluster_assign)
-            obj.cluster_assign = cluster_assign;
-            obj.number_cluster = max(unique(cluster_assign));
+        function set_cluster_assign(dg, cluster_assign)
+            if length(cluster_assign) ~= dg.sample_size
+                error("the number of the assignment vector must be the same as the sample size")
+            end
+
+            dg.cluster_assign = cluster_assign;
+            dg.number_cluster = max(unique(dg.cluster_assign));
+        end
+        
+        function get_cluster_mean(dg)
+            dg.cluster_mean_mat = zeros(dg.dimension, dg.number_cluster);
+            for i = 1:dg.number_cluster
+                data_cluster = dg.data(:, (dg.cluster_assign ==  i));
+                dg.cluster_mean_mat(:,i) = mean(data_cluster, 2);
+            end
+        end
+
+        function get_cluster_mean_sparse(dg)
+            dg.cluster_mean_sparse_mat = zeros(dg.number_entries_survived, dg.number_cluster);
+            for i = 1:dg.number_cluster
+                data_cluster_sparse = dg.data(dg.entries_survived_boolean, (dg.cluster_assign ==  i));
+                dg.cluster_mean_sparse_mat(:,i) = mean(data_cluster_sparse, 2);
+            end
         end
         
  
