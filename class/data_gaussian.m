5f7acc12829160af82ee36bcba479d52fa725b97
Jong-Min-Moon <jm.moon@yonsei.ac.kr>
2024-03-27 20:20:36 -0700

    data_gaussian basics

--- class/data_gaussian.m
+++ class/data_gaussian.m
@@ -5,4 +5,13 @@ classdef data_gaussian
         data
         sample_covariance
     end
+
+    methods
+        function obj = data_gaussian(data_matrix)
+            obj.data = data_matrix;
+            obj.dimension = size(obj.data,1);
+            obj.sample_size = size(obj.data,2);
+            obj.sample_covariance = obj.data * obj.data' / (obj.sample_size-1);
+        end
+    end
 end
