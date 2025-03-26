f74b0e464ca106bd5c9ff755abd0043af755eeb1
Jong-Min-Moon <jm.moon@yonsei.ac.kr>
2025-03-26 00:34:08 -0700

    let's just try running

--- bandit/block_replication_for_server_bandit.m
+++ bandit/block_replication_for_server_bandit.m
@@ -16,8 +16,9 @@ classdef block_replication_for_server_bandit < handle
                 else
                     data_obj_now = blfs.data_obj(x_noisy, blfs.omega_sparsity);
                 end
-                blfs.learner = iterative_kmeans(data_obj_now, blfs.number_cluster, blfs.data_generator.conditional_correlation, blfs.init_method);
-                blfs.learner.run_iterative_algorithm(blfs.n_iter_max, blfs.window_size_half, 0.01, blfs.run_full, blfs.loop_detect_start);
+                blfs.learner = sdp_kmeans_bandit_simul(data_obj_now, blfs.number_cluster);
+                % = iterative_kmeans(data_obj_now, blfs.number_cluster, blfs.data_generator.conditional_correlation, blfs.init_method);
+                blfs.learner.run_iterative_algorithm(blfs.n_iter_max, blfs.cluster_true);
     
                 database_subtable = blfs.learner.get_database_subtable(rep, blfs.data_generator.separation, blfs.data_generator.conditional_correlation, blfs.data_generator.support, blfs.cluster_true, blfs.data_generator.sparse_precision_matrix);
         end%end of run_one_replication
