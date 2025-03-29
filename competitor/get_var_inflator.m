fb0c1709b32df08138716165d54a4bb572622184
Jong-Min-Moon <jm.moon@yonsei.ac.kr>
2025-03-28 19:24:25 -0700

    wasserman implementation complete

---
+++ competitor/get_var_inflator.m
@@ -0,0 +1,3 @@
+function alpha = get_alpha_inflator(n, p)
+
+end % end of method get_alpha
---
+++ competitor/variance_test_spectral.m
@@ -0,0 +1,74 @@
+classdef variance_test_spectral < handle
+
+    properties
+        X           % Data matrix (d x n)
+        K           % Number of clusters
+        n           % Number of data points
+        p           % Data dimension
+
+        var_coordinatewise         % p-array: coordinate-wise variance
+        var_min
+        alpha       % variance scaling
+
+        acc_dict
+        cluster_est_dict
+        signal_entry_est
+
+        x_tilde_est       
+        omega_est_time    
+        sdp_solve_time    
+        entries_survived  
+        obj_val_prim     
+        obj_val_dual      
+        obj_val_original  
+    end
+
+    methods
+        function obj = variance_test_spectral(X)
+
+            if ~ismatrix(X) || ~isnumeric(X)
+                error('Input X must be a numeric matrix.');
+            end
+
+
+            obj.X = X;
+            obj.K = 2;
+            obj.n = size(X, 2);
+            obj.p = size(X, 1);
+            obj.var_coordinatewise = repmat(NaN,1,obj.p);
+
+        end % end of initialization
+
+
+
+
+        function signal_entry_est = get_signal_entry_est(obj)
+            %If A is a matrix whose columns are random variables and whose rows are observations, then V is a row vector containing the variance corresponding to each column.
+            obj.var_coordinatewise  = var(obj.X');
+            obj.var_min = min(obj.var_coordinatewise);
+            log_np = log( obj.n * obj.p);
+            alpha = sqrt(6 * ( log_np / obj.n)) + (2 * log_np / obj.n);
+            min_var_scaled = obj.var_min * (1+alpha)/(1-alpha);
+            signal_entry_est =  obj.var_coordinatewise > min_var_scaled;
+            obj.signal_entry_est = signal_entry_est;
+        end % end of method get_signal_entry_est
+
+
+
+        function fit_predict(obj)
+            obj.get_signal_entry_est();
+            X_sub = obj.X(obj.signal_entry_est,:);
+            mean_sub = mean(X_sub')';
+            sample_cov_sub = cov(X_sub');
+            [V, D] = eig(sample_cov_sub);
+            V
+            v1 = V(:,end)
+            cluster_est = (X_sub-mean_sub)' * v1 > 0
+
+            
+
+        end % end of fit_predict
+        
+   
+    end % end of methods
+end
