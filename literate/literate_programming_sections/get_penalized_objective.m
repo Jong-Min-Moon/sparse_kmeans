3905b295b6219cdb3115b82a00fad82b7dbcd631
Jong-Min-Moon <jm.moon@yonsei.ac.kr>
2025-04-23 12:05:11 -0700

    beta penalty

--- literate/literate_programming_sections/get_penalized_objective.m
+++ literate/literate_programming_sections/get_penalized_objective.m
@@ -7,10 +7,24 @@ function obj = get_penalized_objective(X, G)
 % * X: p x n data matrix (usually a truncated matrix, where p is |S| where S 
 % is selected variables) )
 % * G: 1 x n vector of cluster labels in {1,...,K}
-% Computes the full profile likelihood objective
-% X: p x n data matrix
-% G: 1 x n vector of cluster labels
-[p,n] = size(X);
-lik_obj = get_likelihood_objective(X, G);      % reuse core SDP component
-obj = lik_obj + n*p;
+% Computes the penalized objective combining the profile likelihood 
+% and squared L2 distance between cluster means.
+%
+% Inputs:
+%   X : p x n data matrix
+%   G : 1 x n vector of cluster labels
+%
+% Output:
+%   obj : scalar value of penalized objective
+    [p, n] = size(X);
+    % Reuse core likelihood component
+    lik_obj = get_likelihood_objective(X, G);    
+    % Compute cluster means
+    cluster_mean_one = mean(X(:, G == 1), 2);  % p x 1
+    cluster_mean_two = mean(X(:, G == 2), 2);  % p x 1
+    % Compute squared L2 distance between cluster means
+    diff = cluster_mean_one - cluster_mean_two;
+    penalty = sum(diff .^ 2);
+    % Combine likelihood and penalty
+    obj = lik_obj + penalty;
 end
