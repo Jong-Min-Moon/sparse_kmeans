f7d26e0940d45b15a79508a2f4241faeabb8bba0
Jong-Min-Moon <jm.moon@yonsei.ac.kr>
2025-04-25 16:50:30 -0700

    demeaned penalty

--- literate/literate_programming.m
+++ literate/literate_programming.m
@@ -863,7 +863,7 @@ end
 % * G: 1 x n vector of cluster labels in {1,...,K}
 function obj = get_penalized_objective(X, G)
 % Computes the penalized objective combining the profile likelihood 
-% and squared L2 distance between cluster means.
+% and squared Frobenius norm of the row-centered data matrix.
 %
 % Inputs:
 %   X : p x n data matrix
@@ -872,17 +872,11 @@ function obj = get_penalized_objective(X, G)
 % Output:
 %   obj : scalar value of penalized objective
     [p, n] = size(X);
-    n1 = sum(G==1);
-    n2 = sum(G==2);
-    sd_noise_entry = (n / (n1*n2));
-    % Reuse core likelihood component
+    
+    % Core likelihood component
     lik_obj = get_likelihood_objective(X, G);    
-    % Compute cluster means
-    cluster_mean_one = mean(X(:, G == 1), 2);  % p x 1
-    cluster_mean_two = mean(X(:, G == 2), 2);  % p x 1
-    % Compute squared L2 distance between cluster means
-    diff = cluster_mean_one - cluster_mean_two;
-    penalty = n * sum(diff .^ 2) / sd_noise_entry;
+    % Compute penalty using variance
+    penalty = 2 * (n - 1) * sum(var(X, 0, 2)); 
     % Combine likelihood and penalty
     obj = lik_obj + penalty;
 end
