da9baf462d485c44f5ccb9bdd06d442d2f1bde99
Jong-Min-Moon <45618311+Jong-Min-Moon@users.noreply.github.com>
2025-07-27 10:33:36 -0700

    create slot for sdp solver

--- code_basic/sdp_to_cluster.m
+++
@@ -1,7 +0,0 @@
-function cluster_est_now = sdp_to_cluster(Z_now, K) 
-
-    [U_sdp,~,~] = svd(Z_now);
-    U_top_k = U_sdp(:,1:K);
-    [cluster_est_now,~] = kmeans(U_top_k,K);  % label %column vaector
-    %cluster_est_now = cluster_est_now .* (cluster_est_now ~= 2) + (cluster_est_now == 2)* (-1);    
-    cluster_est_now = cluster_est_now';   %row vector
