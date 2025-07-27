8c9eb6c31ac9bea00a0e30083fa2f6ad595be53b
Jong-Min-Moon <45618311+Jong-Min-Moon@users.noreply.github.com>
2025-07-27 02:55:14 -0700

    noniso approx sparse mean ready

--- literate/literate_programming.m
+++ literate/literate_programming.m
@@ -2307,6 +2307,7 @@ classdef data_generator_correlated_approximately_sparse_mean < data_generator_t_
              % Create mean matrix
              mean_matrix = [repmat(mu1', obj.n1, 1); repmat(mu2', obj.n2, 1)];
              mean_matrix= mean_matrix';
+             obj.mean_matrix = mean_matrix;
         end
        function noise_matrix = get_noise_matrix(obj) %modification: t noise -> Gaussian noise
             % Generate noise once
@@ -2314,6 +2315,7 @@ classdef data_generator_correlated_approximately_sparse_mean < data_generator_t_
             noise_matrix = mvnrnd(zeros([obj.n, obj.p]), obj.Sigma); %$Gaussian noise
             noise_matrix = noise_matrix'; % p x n matrix
             noise_matrix = sqrtm(obj.Sigma) * noise_matrix;
+            obj.noise_matrix = noise_matrix;
  
         end        
         function [X,label] = get_data(obj, delta)
--- literate/literate_programming_sections/data_generator_correlated_approximately_sparse_mean.m
+++ literate/literate_programming_sections/data_generator_correlated_approximately_sparse_mean.m
@@ -18,6 +18,7 @@ classdef data_generator_correlated_approximately_sparse_mean < data_generator_t_
              % Create mean matrix
              mean_matrix = [repmat(mu1', obj.n1, 1); repmat(mu2', obj.n2, 1)];
              mean_matrix= mean_matrix';
+             obj.mean_matrix = mean_matrix;
         end
        function noise_matrix = get_noise_matrix(obj) %modification: t noise -> Gaussian noise
             % Generate noise once
@@ -25,6 +26,7 @@ classdef data_generator_correlated_approximately_sparse_mean < data_generator_t_
             noise_matrix = mvnrnd(zeros([obj.n, obj.p]), obj.Sigma); %$Gaussian noise
             noise_matrix = noise_matrix'; % p x n matrix
             noise_matrix = sqrtm(obj.Sigma) * noise_matrix;
+            obj.noise_matrix = noise_matrix;
  
         end        
         function [X,label] = get_data(obj, delta)
