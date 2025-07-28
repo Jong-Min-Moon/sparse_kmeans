69cf1653d50a8496f2abd3686018ff388fddfc6d
Jong-Min-Moon <45618311+Jong-Min-Moon@users.noreply.github.com>
2025-07-27 19:26:57 -0700

    approx sparse precision ready

--- literate/literate_programming.m
+++ literate/literate_programming.m
@@ -2451,7 +2451,10 @@ classdef data_generator_approximately_sparse_precision < data_generator_t
     methods
         function get_cov(obj, delta)
             obj.precision = get_precision_band(obj.p, 2, 0.45);
-            obj.precision(obj.precision==0) =   delta;
+for i = 1:obj.p - 2
+    obj.precision(i, i+2) = delta;   % Upper second off-diagonal
+    obj.precision(i+2, i) = delta;   % Lower second off-diagonal
+end
             obj.Sigma = inv(obj.precision);
         end
     
@@ -2460,7 +2463,7 @@ classdef data_generator_approximately_sparse_precision < data_generator_t
             obj.get_cov(delta);
             label = obj.get_cluster_label();
             mean_matrix= obj.get_mean_matrix();
-                rng(obj.seed);
+             rng(obj.seed);
             X = mvnrnd(mean_matrix', obj.Sigma);
             X=X';
         end
