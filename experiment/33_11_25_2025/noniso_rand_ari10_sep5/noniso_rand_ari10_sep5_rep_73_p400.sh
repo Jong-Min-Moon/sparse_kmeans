2c17943f20c5b17259e1484859daf70f4d2eef57
jongmin mun usc cluster <jongminm@usc.edu>
2026-01-10 17:22:52 -0800

    so far
    Signed-off-by: jongmin mun usc cluster <jongminm@usc.edu>

---
+++ experiment/33_11_25_2025/noniso_rand_ari10_sep5/noniso_rand_ari10_sep5_rep_73_p400.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --output="/home1/jongminm/sparse_kmeans/experiment/33_11_25_2025/noniso_rand_ari10_sep5/noniso_rand_ari10_sep5_rep_73_p400.out"
+#SBATCH --partition=main
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=8G
+#SBATCH --time=3:59:59
+
+# Echo job start time and host
+echo "Starting job for rep=73 on $(hostname) at $(date)"
+
+# Load necessary modules
+module purge
+module load legacy/CentOS7
+module load matlab/2022a
+
+# Change to base directory
+cd "/home1/jongminm/sparse_kmeans/experiment/33_11_25_2025/noniso_rand_ari10_sep5"
+
+# Run MATLAB script in batch mode
+matlab -batch noniso_rand_ari10_sep5_rep_73_p400
+
+# Echo job finish time
+echo "Finished job for rep=73 at $(date)"
