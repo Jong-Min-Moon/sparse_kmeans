8058226dd447d91d6c21031f9de801333dd8ca4d
jongmin mun usc cluster <jongminm@usc.edu>
2026-01-10 20:44:37 -0800

    bandit C simul start
    Signed-off-by: jongmin mun usc cluster <jongminm@usc.edu>

--- experiment/34_01_16_2025/run_bandit_task.m
+++ experiment/34_01_16_2025/run_bandit_task.m
@@ -1,19 +1,18 @@
-function run_bandit_task(task_id, param_file, results_dir) % Add
-    repository paths addpath(genpath('/home1/jongminm/sparse_kmeans'));
-addpath(genpath('../../main_code'));
+function run_bandit_task(task_id, param_file, results_dir) 
+    addpath(genpath('/home1/jongminm/sparse_kmeans'));
 
 params = readmatrix(param_file);
 C_val = params(task_id, 1);
 p_val = params(task_id, 2);
 rep = params(task_id, 3);
 
-% Run simulation s = 10;
-support = 1 : s;
-[ data, label_true ] = generate_gaussian_data(
-    200, p_val, s, 4, 'iso', 'equal_symmetric', 0, rep, 0.5, rep);
+% Run simulation 
+s = 10;
+support = 1:s;
+[ data, label_true ] = generate_gaussian_data(200, p_val, s, 4, 'iso', 'equal_symmetric', 0, rep, 0.5, rep);
 
     bandit = sdp_kmeans_bandit(data', 2, C_val);
-    bandit.fit_predict(20, label_true');
+    bandit.fit_predict(1000, label_true');
     
     % Save result to individual .mat file
     bandit.save_results(results_dir, rep, 4, support);
